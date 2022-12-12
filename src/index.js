b0b7d73a5cd88050925567676b65c7f27200d32d
Danik <95442609+BanduraDanik1@users.noreply.github.com>
2022-12-12 21:42:24 +0200

    add fix

--- src/index.js
+++ src/index.js
@@ -1,205 +1,110 @@
-import './css/main.css';
+
+
 import axios from 'axios';
-import Notiflix from 'notiflix';
-const debounce = require('lodash.debounce');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const searchForm = document.querySelector('.search-form');
-const submitBtn = document.querySelector("[type='submit']");
+const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
-const footer = document.querySelector('.footer');
+const loadMore1 = document.querySelector('.load-more');
+const btnMore = document.querySelector('.btn--more');
 
-const PIXABAY_URL = 'https://pixabay.com/api/';
-const PIXABAY_API_KEY = '32024514-4783314e47ab08567e2df8ea6';
-const DEBOUNCE_DELAY = 100;
-let currentPage = 1;
-const POST_PER_PAGE = 40;
+console.log(btnMore);
+let searchQuery;
+let pageNumber = 1;
+const countImg = 40;
 
-searchForm.addEventListener(
-  'input',
-  debounce(event => {
-    const searchValue = event.target.value;
-    localStorage.setItem('search-term', searchValue.trim());
-  }, DEBOUNCE_DELAY)
-);
+console.log(btnMore);
 
-submitBtn.addEventListener('click', event => {
-  event.preventDefault();
-  const savedSearch = localStorage.getItem('search-term');
-  if (savedSearch === null || savedSearch === '') {
-    Notiflix.Notify.info('Please type something in the search input.');
-    return;
-  }
-  currentPage = 1;
-  fetchImages(savedSearch, currentPage);
-});
+form.addEventListener('submit', onSubmitForm);
 
-const fetchImages = async (searchValue, currentPage) => {
-  let galleryMarkup = '';
-  let params = new URLSearchParams({
-    key: PIXABAY_API_KEY,
-    q: searchValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: POST_PER_PAGE,
-  });
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  searchQuery = evt.currentTarget.elements.searchQuery.value;
+  console.log(searchQuery);
+  pageNumber = 1;
+  getUser();
+}
 
+async function getUser() {
   try {
-    const response = await axios.get(
-      `${PIXABAY_URL}?${params}&page=${currentPage}`
+    const resp = await axios.get(
+      `https://pixabay.com/api/?key=31643149-fa666e9d7417fd7b721c14976`,
+      {
+        params: {
+          q: `${searchQuery}`,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: `${countImg}`,
+          page: `${pageNumber}`,
+        },
+      }
     );
-    // console.log('response: ', response);
-    const imagesArray = response.data.hits;
-    // console.log('imagesArray: ', imagesArray);
-    if (searchValue === '') {
+    const totalHits = resp.data.totalHits;
+
+    onMarkup(resp.data.hits);
+    if (pageNumber === 1 && resp.data.hits.length) {
+      btnMore.classList.add('is-hidden');
+      Notify.success(`Hooray! We found ${totalHits} images.`);
     }
-    if (imagesArray.length === 0) {
-      Notiflix.Notify.failure(
+    if (!resp.data.hits.length) {
+      btnMore.classList.remove('is-hidden');
+      return Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-
-      return;
     }
 
-    loadMoreBtn.style.display = 'block';
-    footer.style.display = 'flex';
-
-    galleryMarkup = imagesArray
-      .map(image => {
-        return `
-        <div class="photo-card">
-          <div class ="thumb">
-            <img class="img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-          </div>
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
-              ${image.likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>
-              ${image.views}
-            </p>
-            <p class="info-item">
-              <b>Comments</b>
-              ${image.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>
-              ${image.downloads}
-            </p>
-          </div>
-        </div>
-      `;
-      })
-      .join('');
-
-    gallery.innerHTML = galleryMarkup;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const fetchNewImages = async (searchValue, currentPage) => {
-  let galleryMarkup = '';
-  let params = new URLSearchParams({
-    key: PIXABAY_API_KEY,
-    q: searchValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: POST_PER_PAGE,
-  });
-
-  try {
-    const response = await axios.get(
-      `${PIXABAY_URL}?${params}&page=${currentPage}`
-    );
-    // console.log('response: ', response);
-    const imagesArray = response.data.hits;
-    // console.log('imagesArray: ', imagesArray);
-    if (imagesArray.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
-    }
-
-    galleryMarkup = imagesArray
-      .map(image => {
-        return `
-        <div class="photo-card">
-          <div class ="thumb">
-            <img class="img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-          </div>
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
-              ${image.likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>
-              ${image.views}
-            </p>
-            <p class="info-item">
-              <b>Comments</b>
-              ${image.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>
-              ${image.downloads}
-            </p>
-          </div>
-        </div>
-      `;
-      })
-      .join('');
-
-    gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-loadMoreBtn.addEventListener('click', async () => {
-  const searchValue = localStorage.getItem('search-term');
-  currentPage++;
-  let params = new URLSearchParams({
-    key: PIXABAY_API_KEY,
-    q: searchValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: POST_PER_PAGE,
-  });
-  try {
-    const response = await axios.get(
-      `${PIXABAY_URL}?${params}&page=${currentPage}`
-    );
-
-    const imagesArray = response.data.hits;
-    const imagesPerPage = POST_PER_PAGE;
-
-    const totalImages = response.data.totalHits;
-    const maxPageNumber = totalImages / imagesPerPage;
-    const maxPageNumberRoundUp = Math.ceil(maxPageNumber);
-    console.log('currentPage: ', currentPage);
-    console.log('maxPageNumber: ', maxPageNumber);
-    console.log('maxPageNumberRoundUp: ', maxPageNumberRoundUp);
-
-    if (currentPage === maxPageNumberRoundUp) {
-      footer.style.display = 'none';
-      loadMoreBtn.style.display = 'none';
-      Notiflix.Notify.info(
+    if (totalHits <= pageNumber * countImg) {
+      btnMore.classList.remove('is-hidden');
+      return Notify.warning(
         "We're sorry, but you've reached the end of search results."
       );
     }
 
-    fetchNewImages(searchValue, currentPage);
-  } catch (error) {
-    console.error(error);
+    btnMore.addEventListener('click', onLoadMoreBtn);
+
+    return;
+  } catch (err) {
+    console.error(err);
   }
-});
-footer.style.display = 'none';
-loadMoreBtn.style.display = 'none';
+}
+
+function onMarkup(arr) {
+  const {
+    likes,
+    views,
+    comments,
+    downloads,
+    webformatURL,
+    largeImageURL,
+    tags,
+  } = arr;
+  const markup = arr
+    .map(item => {
+      return `<div class="photo-card"> 
+      <div class ="thumb">
+        <img class = "img" src="${item.webformatURL}" alt="${item.tags}" width="340" height="225" loading="lazy" />
+      </div>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${item.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${item.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${item.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${item.downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+  gallery.innerHTML = markup;
+}
+function onLoadMoreBtn() {
+  pageNumber += 1;
+  getUser();
+}
